fb89e4d588f3810de8df6715d1381adef50f6557
Nico <iselinico@bluewin.ch>
2025-08-20 21:38:13 +0200

    Create Button-Component

---
+++ src/lib/components/button/Button.svelte
@@ -0,0 +1,31 @@
+<!-- 
+=====================================================================
+  BUTTON
+=====================================================================
+  This file defines the Button-Component of the project
+-->
+
+<!--SCRIPTS-->
+<script>
+	import { _ } from 'svelte-i18n';
+
+	// Initialize Variables
+	export let label;
+	export let action;
+
+    // Action
+	function handleClick(e) {
+		if (action) action(e);
+	}
+</script>
+
+<!--STYLES-->
+<style>
+	@import './button.css';
+</style>
+
+<!--MARKUP-->
+<button id="primary-button" on:click={handleClick}>
+	<!-- Label -->
+	<span>{label}</span>
+</button>
